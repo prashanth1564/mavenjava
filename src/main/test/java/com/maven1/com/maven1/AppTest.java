9ac66d9b610a893e8c6724c0b76b7e8198355727
prashanth1564 <prashanthbanja@gmail.com>
2023-12-04 19:47:40 +0530

    1st commit

---
+++ src/main/test/java/com/maven1/com/maven1/AppTest.java
@@ -0,0 +1,21 @@
+package com.maven1.com.maven1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("uiasbd iuahsod ioahsd");
+    }
+}
